757aaaadd31b2bf300417825fd4b1aa99745a1fd
Sahil Sahikh <sahilsk565678@gmail.com>
2024-02-12 02:02:24 +0530

    Add controller and routes to get videoById, update, delete, togglepublish

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -2,7 +2,7 @@ import { Video } from "../schemas/videos.schema.js";
 import ApiError from "../utils/ApiError.js";
 import ApiSuccess from "../utils/ApiSuccess.js";
 import asyncHandler from "../utils/asyncHandler.js";
-import uploadCloudinary from "../utils/cloudinary.js";
+import uploadCloudinary, { deleteCloudinary } from "../utils/cloudinary.js";
 
 
 export const getAllVideo = asyncHandler(async (req, res) => {
@@ -11,7 +11,7 @@ export const getAllVideo = asyncHandler(async (req, res) => {
     page = Number(page)
     limit = Number(limit)
 
-    const baseQuery = { isPublished: true}
+    const baseQuery = { isPublished: true }
 
     if (query) {
         const regExp = new RegExp(query, 'i')
@@ -26,31 +26,31 @@ export const getAllVideo = asyncHandler(async (req, res) => {
     const allowedSortOptions = ['uploadDate', 'views'];
 
     if (sortBy && !allowedSortOptions.includes(sortBy)) {
-        throw new ApiError(403,`Invalid sortBy parameter. Allowed values are: ${allowedSortOptions.join(', ')}`);
+        throw new ApiError(403, `Invalid sortBy parameter. Allowed values are: ${allowedSortOptions.join(', ')}`);
     }
 
     let sort = {};
-        switch (sortBy) {
-            case allowedSortOptions[0]:
-                sort.createdAt = sortType === 'desc' ? -1 : 1;
-                break;
-            case allowedSortOptions[1]:
-                sort.views = sortType === 'desc' ? -1 : 1;
-                break;
+    switch (sortBy) {
+        case allowedSortOptions[0]:
+            sort.createdAt = sortType === 'desc' ? -1 : 1;
+            break;
+        case allowedSortOptions[1]:
+            sort.views = sortType === 'desc' ? -1 : 1;
+            break;
 
-            default:
-                // You can define your own relevance algorithm or fallback to a default sorting
-                // Here, we'll sort by createdAt as a default option
-                sort.createdAt = sortType === 'desc' ? -1 : 1;
-                break;
-        }
+        default:
+            // You can define your own relevance algorithm or fallback to a default sorting
+            // Here, we'll sort by createdAt as a default option
+            sort.createdAt = sortType === 'desc' ? -1 : 1;
+            break;
+    }
 
-    const videoList = await Video.find(baseQuery).sort(sort).skip( (page-1)* limit).limit(limit)
+    const videoList = await Video.find(baseQuery).sort(sort).skip((page - 1) * limit).limit(limit)
 
     const totalCount = videoList.length
 
     return res.status(200)
-        .json( new ApiSuccess({videoList,page,totalCount}))
+        .json(new ApiSuccess({ videoList, page, totalCount }))
 
 
 })
@@ -90,4 +90,98 @@ export const publishAVideo = asyncHandler(async (req, res) => {
     return res.status(200)
         .json(new ApiSuccess(isUploadVideo, 'Video is Uploaded Successfully !!!'))
 
+})
+
+export const getVideoById = asyncHandler(async (req, res) => {
+
+    const { videoId } = req.params
+
+    const video = await Video.findById({ _id: videoId }).populate({
+        path: 'owner',
+        select: 'username fullName email'
+    })
+
+    if (!video) throw new ApiError(400, "Video Not found !!!")
+
+    return res.status(200)
+        .json(new ApiSuccess(video, 'Video fetched Successfully !!!'))
+})
+
+export const updateVideo = asyncHandler(async (req, res) => {
+    let { videoId } = req.params
+    const loginUser = req.user._id
+
+    const video = await Video.findById({ _id: videoId }).populate({
+        path: 'owner',
+        select: 'username fullName email'
+    })
+
+    if (!video) throw new ApiError(400, "Video Not found !!!")
+
+    if (loginUser.toString() !== video.owner.toString()) throw new ApiError(401, "Unauthorized Request!!!")
+
+    const { title, description } = req.body
+
+    if (title) video.title = title
+    if (description) video.description = description
+
+    const thumbnailLocalPath = req.file?.path
+
+    if (thumbnailLocalPath) {
+        const isPreviousDelete = await deleteCloudinary(video.thumbnail, 'image')
+        if (isPreviousDelete) {
+
+            const thumbnail = await uploadCloudinary(thumbnailLocalPath)
+            video.thumbnail = thumbnail.url
+        }
+    }
+
+    await video.save({ validateBeforeSave: false })
+
+    const updateVideo = await Video.findById(video._id)
+
+    return res.status(200)
+        .json(new ApiSuccess(updateVideo, "Video Details Updated Successfully !!!"))
+
+})
+
+export const deleteVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    const loginUser = req.user._id
+
+    const video = await Video.findById(videoId)
+
+    if (!video) throw new ApiError(400, "Video Not found !!!")
+
+    if (loginUser.toString() !== video.owner.toString()) throw new ApiError(401, "Unauthorized Request !!!")
+
+    const isPreviousVideoDeleted = await deleteCloudinary(video.videoFile, 'video')
+    const isPreviousThumbnailDeleted = await deleteCloudinary(video.thumbnail, 'image')
+
+    if (!(isPreviousThumbnailDeleted, isPreviousVideoDeleted)) {
+        throw new ApiError(500, "Something went wrong while Deleting Video !!!")
+    }
+
+    await video.deleteOne({ _id: video._id })
+    return res.status(200)
+        .json(new ApiSuccess({ isVideoDeleted: true }, 'Video is Successfully Deleted !!!'))
+
+})
+
+export const togglePublishStatus = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    const loginUser = req.user._id
+
+    const video = await Video.findById(videoId)
+
+    if (!video) throw new ApiError(400, "Video Not found !!!")
+
+    if (loginUser.toString() !== video.owner.toString()) throw new ApiError(401, "Unauthorized Request !!!")
+
+    video.isPublished = !video.isPublished
+
+    video.save({ validateBeforeSave: false })
+
+    return res.status(200)
+        .json(new ApiSuccess({ isPublished: video.isPublished }, "Toggle Publish Status Successfully !!!"))
 })
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,25 +1,33 @@
 import { Router } from "express";
 import { uploadFile } from "../middlewares/multer.middleware.js";
-import { getAllVideo, publishAVideo } from "../controllers/video.controller.js";
+import { deleteVideo, getAllVideo, getVideoById, publishAVideo, togglePublishStatus, updateVideo } from "../controllers/video.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.use(verifyJWT)
 
-router.route('/').get(getAllVideo).post(
-    uploadFile.fields([
-        {
-            name: 'videoFile',
-            maxCount: 1
-        },
-        {
-            name: 'thumbnail',
-            maxCount: 1
-        }
-    ]),
-    publishAVideo
-)
+router.route('/')
+    .get(getAllVideo)
+    .post(
+        uploadFile.fields([
+            {
+                name: 'videoFile',
+                maxCount: 1
+            },
+            {
+                name: 'thumbnail',
+                maxCount: 1
+            }
+        ]),
+        publishAVideo
+    )
 
+router.route('/:videoId')
+    .get(getVideoById)
+    .patch(uploadFile.single("thumbnail"), updateVideo)
+    .delete(deleteVideo)
+
+router.route("/toggle/publish/:videoId").patch(togglePublishStatus)
 
 export default router
